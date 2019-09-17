04e2334f5db6af3da4d2ffa85208936d264622f4
Daniel Wzoreck <wzoreck2000@gmail.com>
2019-09-17 10:28:52 -0300

    07 corrigida

--- lista_04_condicional/07.c
+++ lista_04_condicional/07.c
@@ -4,10 +4,13 @@ int main(){
     int n1, n2, n3;
     printf("Digite três números: ");
     scanf("%d" "%d" "%d", &n1, &n2, &n3);
-    if(n1 > n2 && n1 > n2){
 
-
-
+    if(n1>n2 && n1>n3){
+        printf("%d é o maior número!", n1);
+    } else if(n2>n1 && n2>n3){
+        printf("%d é o maior número!", n2);
+    } else if(n3>n1 && n3>n2){
+        printf("%d é o maior número!", n3);
     }
 
     return 0;
